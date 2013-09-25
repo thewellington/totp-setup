843e99bf9c5a89e2336a43eb4ee1835e4f6d3b8f
W. S. Wellington <bwellington@fulcrum.net>
2013-09-25 11:46:25 -0700

    update .9 for better echoing
    Signed-off-by: W. S. Wellington <bwellington@fulcrum.net>

--- scripts/totp-user-setup.sh
+++ scripts/totp-user-setup.sh
@@ -8,7 +8,7 @@
 # Works well on CentOS 6.x
 #
 # by bill@wellingtonnet.net for Fulcrum Technologies Inc.
-# v0.8 2013-09-20
+# v0.9 2013-09-25
 #
 
 OUTFILE='/etc/users.oath'
@@ -28,20 +28,20 @@ echo -e $OUTPUT >> $OUTFILE
 
 # Output to User
 clear
-echo -e '\r\r\r
-\033[1;31m----- BEGIN SECRET INFORMATION -----\e[0m
-Authentication Token has been generated for $EMAIL
-This information needs to be kept secret, do not share this with anybody.
-You should save this information in a secure place - do not leave this lying around!
-\r
-Your new secret key is: $SECRET32
-\r
-1. Download Google Authenticator to your mobile device, or use an alternate app capable of generating TOTP tokens.
-2. Copy the following URL and paste it into a web browser to generate a QR code.
-3. Scan the QR code with your mobile device.
-\r
-QR Code URL:
-https://www.google.com/chart?chs=400x400&chld=M|08&cht=qr&chl=otpauth://totp/$USERNAME@$HOSTNAME%3Fsecret%3D$SECRET32
-\r
-\033[1;31m----- END SECRET INFORMATION -----\e[0m
-\r\r\r'
+echo -e '\r\r\r'
+echo -e '\033[1;31m----- BEGIN SECRET INFORMATION -----\e[0m'
+echo -e 'Authentication Token has been generated for '$EMAIL
+echo -e 'This information needs to be kept secret, do not share this with anybody.'
+echo -e 'You should save this information in a secure place - do not leave this lying around!'
+echo -e '\r'
+echo -e 'Your new secret key is: '$SECRET32
+echo -e '\r'
+echo -e '1. Download Google Authenticator to your mobile device, or use an alternate app capable of generating TOTP tokens.'
+echo -e '2. Copy the following URL and paste it into a web browser to generate a QR code.'
+echo -e '3. Scan the QR code with your mobile device.'
+echo -e '\r'
+echo -e 'QR Code URL:'
+echo -e 'https://www.google.com/chart?chs=400x400&chld=M|08&cht=qr&chl=otpauth://totp/'$USERNAME'@'$HOSTNAME'%3Fsecret%3D'$SECRET32
+echo -e '\r'
+echo -e '\033[1;31m----- END SECRET INFORMATION -----\e[0m'
+echo -e '\r\r\r'
